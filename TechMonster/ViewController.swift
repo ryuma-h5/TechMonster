7cb76458b8528220ad295aec10d91be7f19b1ee6
Ryuma.h <ryuma810dragon@gmail.com>
2019-04-19 15:51:40 +0900

    Initial Commit

---
+++ TechMonster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechMonster
+//
+//  Created by Ryuma Harada on 2019/04/19.
+//  Copyright © 2019 Ryuma Harada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
